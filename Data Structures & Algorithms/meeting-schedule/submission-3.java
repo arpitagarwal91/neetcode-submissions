33a892a733a04e6df2ee653a884f14d08cb71f8f
Arpit  Agarwal <112945845+arpitagarwal91@users.noreply.github.com>
2026-07-02 00:47:36 +0530

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.java
@@ -0,0 +1,23 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if(intervals.size()==0) return true;
+        Collections.sort(intervals, (a,b) -> a.start==b.start ? a.end - b.end : a.start - b.start);
+        int lastEnd = intervals.get(0).end;
+        for(int i=1;i<intervals.size();i++){
+            if(intervals.get(i).start<lastEnd) return false;
+            lastEnd = intervals.get(i).end;
+        }
+        return true;
+    }
+}
