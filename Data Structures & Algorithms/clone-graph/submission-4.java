1764fcf577d9b9753df58cd703f5bb5149fc3973
Arpit  Agarwal <112945845+arpitagarwal91@users.noreply.github.com>
2026-07-05 14:03:03 +0530

    Add: clone-graph - submission-4

---
+++ Data Structures & Algorithms/clone-graph/submission-4.java
@@ -0,0 +1,37 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        Map<Node, Node> mp = new HashMap<>();
+        if(node==null) return null;
+        return dfs(node, mp);
+    }
+
+    public Node dfs(Node node, Map<Node, Node> mp){
+        if(mp.containsKey(node)) return mp.get(node);
+        Node copy = new Node(node.val);
+        mp.put(node, copy);
+        for(Node nei:node.neighbors){
+            copy.neighbors.add(dfs(nei, mp));
+        }
+        return copy;
+    }
+}
