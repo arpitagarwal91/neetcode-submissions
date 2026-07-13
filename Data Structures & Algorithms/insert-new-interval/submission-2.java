6f351a346a5927476d8c94f0fbbb4765a78f675c
Arpit  Agarwal <112945845+arpitagarwal91@users.noreply.github.com>
2026-07-14 00:48:17 +0530

    Add: insert-new-interval - submission-2

---
+++ Data Structures & Algorithms/insert-new-interval/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> res = new ArrayList<>();
+        for(int i=0;i<intervals.length;i++){
+            if(newInterval[1]<intervals[i][0]){
+                res.add(newInterval);
+                for(int j=i;j<intervals.length;j++) res.add(intervals[j]);
+                int ans[][] = new int[res.size()][2];
+                for(int k=0;k<res.size();k++){
+                    ans[k] = res.get(k);
+                }
+                return ans;
+            }
+            else if(intervals[i][1]<newInterval[0]){  //used intervals[i][0] instead of intervals[i][1]
+                res.add(intervals[i]);
+            }
+            else{
+                int start = Math.min(newInterval[0], intervals[i][0]);
+                int end = Math.max(newInterval[1], intervals[i][1]);
+                newInterval = new int[]{start, end}; //Forgot this one instead did res.add(newOverLapInterval);
+            }
+        }
+        res.add(newInterval);
+        int ans[][] = new int[res.size()][2];
+        for(int k=0;k<res.size();k++){
+            ans[k] = res.get(k);
+        }
+        return ans;
+    }
+}
