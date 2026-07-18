c24df8580379dd8682f91b60fea63c0214101213
Arpit  Agarwal <112945845+arpitagarwal91@users.noreply.github.com>
2026-07-18 17:28:56 +0530

    Add: regular-expression-matching - submission-1

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        Map<String, Boolean> cache = new HashMap<>();
+        return dfs(0,0,s,p,cache);
+    }
+
+    public boolean dfs(int i, int j, String s, String p, Map<String, Boolean> cache){
+        if(cache.containsKey(i+"-"+j)) return cache.get(i+"-"+j);
+        if(i>=s.length() && j>=p.length()) return true;
+        if(j>=p.length()) return false;
+
+        boolean match = (i<s.length()) && (s.charAt(i)==p.charAt(j) || p.charAt(j)=='.');
+        if(j+1<p.length() && p.charAt(j+1)=='*'){
+            cache.put(i+"-"+j, dfs(i, j+2, s, p, cache) || (match && dfs(i+1, j, s, p, cache)));
+            return cache.get(i+"-"+j);
+        }
+
+        if(match){
+            cache.put(i+"-"+j, dfs(i+1, j+1, s, p, cache));
+            return cache.get(i+"-"+j);
+        }
+        cache.put(i+"-"+j, false);
+        return cache.get(i+"-"+j);
+    }
+}
